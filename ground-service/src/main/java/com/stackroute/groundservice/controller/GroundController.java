bf114557b5a51a56ae137b0b72c1e300f0043e62
shamonhashmi148 <shamonhashmi148@gmail.com>
2023-08-03 09:41:47 +0530

    Added apis

--- ground-service/src/main/java/com/stackroute/groundservice/controller/GroundController.java
+++ ground-service/src/main/java/com/stackroute/groundservice/controller/GroundController.java
@@ -2,18 +2,18 @@ package com.stackroute.groundservice.controller;
 
 
 import com.stackroute.groundservice.exception.GroundAlreadyExistsException;
+import com.stackroute.groundservice.exception.GroundNotFoundException;
 import com.stackroute.groundservice.model.Ground;
 import com.stackroute.groundservice.service.GroundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/grounds")
+@RequestMapping("/api/v1")
 public class GroundController {
 
     ResponseEntity<?> responseEntity;
@@ -26,7 +26,7 @@ public class GroundController {
         return new ResponseEntity<>(groundService.getAllGround(), HttpStatus.OK);
     }
 
-    @GetMapping("/addground")
+    @PostMapping("/addground")
     public ResponseEntity<?> addGround(@RequestBody Ground ground){
         try {
             if(groundService.addGround(ground)){
@@ -38,5 +38,41 @@ public class GroundController {
         }
         return responseEntity;
     }
+    @GetMapping("/ground/{id}")
+    public ResponseEntity<?> getGroundById(@PathVariable("id") int id){
+        try {
+            Ground ground = groundService.getGroundById(id);
+            if(ground!=null){
+                responseEntity = new ResponseEntity<>(ground,HttpStatus.OK);
+            }
+        }
+        catch (GroundNotFoundException e){
+            responseEntity = new ResponseEntity<>("No Ground Found",HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
+    }
+
+    @GetMapping("/ground/{email}")
+    public ResponseEntity<?> getGroundByOwner(@PathVariable("email") String email){
+        try {
+            List<Ground> groundList = groundService.getGroundByOwnerEmail(email);
+            if(groundList.size()>0){
+                responseEntity = new ResponseEntity<>(groundList,HttpStatus.OK);
+            }
+        }
+        catch (GroundNotFoundException e){
+            responseEntity = new ResponseEntity<>("No Ground Found",HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
+    }
+    @GetMapping("/status/open")
+    public ResponseEntity<?> getAllOpenGrounds() {
+        return new ResponseEntity<>(groundService.getAllOpenGround(), HttpStatus.OK);
+    }
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Ground>> getGroundsByCategory(@PathVariable("category") String category) {
+        return new ResponseEntity<>(groundService.getGroundByCategory(category), HttpStatus.OK);
+    }
+
 
 }
