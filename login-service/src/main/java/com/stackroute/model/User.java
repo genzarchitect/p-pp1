7eaaba006e305bd7ee28f653fa845ee61dd8d33e
Ubuntu <ubuntu@ip-172-31-44-5.ap-south-1.compute.internal>
2023-08-11 16:52:37 +0000

    kafka update

---
+++ login-service/src/main/java/com/stackroute/model/User.java
@@ -0,0 +1,25 @@
+package com.stackroute.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+    public class User {
+        @Id
+        @Column(columnDefinition = "VARCHAR(50)")
+        private String useremail;
+        private String password;
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable=false, unique = true)
+        private UserType name;
+
+
+    }
+
